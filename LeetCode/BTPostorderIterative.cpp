551ac4200662e9fc7cb34fcd601f80d586debe62
Ashutxshj <ashutosh06066@gmail.com>
2024-06-25 16:36:51 +0530

    25-06

---
+++ LeetCode/BTPostorderIterative.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    vector<int> postOrder(Node *node)
+    {
+        // code here
+        vector<int> ans;
+        stack<Node *> s;
+        if (node == nullptr)
+        {
+            return ans;
+        }
+        s.push(node);
+        while (!s.empty())
+        {
+            node = s.top();
+            s.pop();
+            ans.push_back(node->data);
+            if (node->left)
+            {
+                s.push(node->left);
+            }
+            if (node->right)
+            {
+                s.push(node->right);
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
