658c0c5c30d535b01cb48b27e6961198601945ac
Ashutxshj <ashutosh06066@gmail.com>
2024-06-28 12:29:18 +0530

    28-06

--- CodeForces/Chewbacca.cpp
+++ CodeForces/Chewbacca.cpp
@@ -1,20 +1,20 @@
-// May the force be with you
-
-//! Invert = 9-n
-//? Ex. n=4, so 9-4=5
 #include <iostream>
+#include <string>
 using namespace std;
+
 int main()
 {
-    string t;
-    cin >> t;
-    for (long long int i = 0; i < t.size(); i++)
+    string x;
+    cin >> x;
+
+    for (auto &digit : x)
     {
-        long long int digit = t[i] - '0';
-        if (9 - digit < digit)
-        {
-            t[i] = '0' + (9 - digit);
-        }
+        if (digit > '4')
+            digit = '9' - digit + '0';
     }
-    cout << t;
+
+    if (x.front() == '0')
+        x.front() = '9';
+    cout << x << endl;
+    return 0;
 }
