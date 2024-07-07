7d48669014ae2cf3dcffe7a98cb57e79c5e51224
Ashutxshj <ashutosh06066@gmail.com>
2024-07-07 14:50:48 +0530

    07-07

--- LeetCode/WaterBottles.cpp
+++ LeetCode/WaterBottles.cpp
@@ -1,6 +1,8 @@
-class Solution {
+class Solution
+{
 public:
-    int numWaterBottles(int n, int k) {
+    int numWaterBottles(int n, int k)
+    {
         return n < k ? n : k + numWaterBottles(n - k + 1, k);
     }
 };
