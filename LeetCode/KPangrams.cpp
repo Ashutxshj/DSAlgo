5eb7a5eb87067f279382d77728283b140cb81b99
Ashutxshj <ashutosh06066@gmail.com>
2024-07-26 22:29:40 +0530

    26-07

---
+++ LeetCode/KPangrams.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    bool kPangram(string str, int k)
+    {
+        int arr[26] = {0}, count = 0, op = 0;
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (str[i] != ' ')
+            {
+                arr[str[i] - 'a']++;
+                count++;
+            }
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (arr[i] == 0)
+            {
+                op++;
+            }
+        }
+
+        return (k >= op) && (count >= 26);
+    }
+};
