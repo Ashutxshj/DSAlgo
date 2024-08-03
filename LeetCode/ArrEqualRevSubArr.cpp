854e01d8cc44da4c7600faf2d2d00e89345640c6
Ashutxshj <ashutosh06066@gmail.com>
2024-08-03 16:57:21 +0530

    03-08

---
+++ LeetCode/ArrEqualRevSubArr.cpp
@@ -0,0 +1,10 @@
+class Solution
+{
+public:
+    bool canBeEqual(std::vector<int> &target, std::vector<int> &arr)
+    {
+        std::sort(target.begin(), target.end());
+        std::sort(arr.begin(), arr.end());
+        return target == arr;
+    }
+};
