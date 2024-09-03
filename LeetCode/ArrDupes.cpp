75e49647c8512d0dddbe5afce1450a8a26ebeae4
Ashutosh Jha <ashutosh06066@gmail.com>
2024-09-03 14:08:10 +0530

    Blind75_01

--- LeetCode/ArrDupes.cpp
+++ LeetCode/ArrDupes.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
 using namespace std;
 class Solution
 {
 public:
     bool containsDuplicate(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end());
-        bool ans = false;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] == nums[i - 1])
-            {
-                ans = true;
-            }
+        unordered_set<int> st;
+        for(int i=0;i<nums.size();i++){
+            if(st.find(nums[i])!=st.end())
+                return true;
+            
+            st.insert(nums[i]);
         }
-        return ans;
+        return false;   
     }
 };
 int main()
 {
     Solution sol = Solution();
-    vector<int> ans = {1, 2, 1, 4};
+    vector<int> ans = {1, 2, 3, 4};
     cout << sol.containsDuplicate(ans);
 }
