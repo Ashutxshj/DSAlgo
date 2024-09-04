e67207929ce4cbb84bfed37a060b29445065cb67
Ashutosh Jha <ashutosh06066@gmail.com>
2024-09-04 14:56:32 +0530

    NeetCode02

--- LeetCode/StrValidAna.cpp
+++ LeetCode/StrValidAna.cpp
@@ -1,36 +1,34 @@
 
+''' An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 '''
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-'''
-
 
 #include <iostream>
-#include<algorithm>
-#include<unordered_map>
-using namespace std;
-class Solution {
+#include <algorithm>
+#include <unordered_map>
+                                                                                                    using namespace std;
+class Solution
+{
 public:
-    bool isAnagram(string s, string t) {
-        unordered_map<char,int> mp;
-        //Char appears in s, increase count
-        for(auto x:s){
-            mp[x]++;
+    bool isAnagram(string s, string t)
+    {
+        if (s.size() != t.size())
+            return false;
+        unordered_map<char, int> mpp;
+        for (int i = 0; i < s.size(); i++)
+        {
+            mpp[s[i]]++;
+            mpp[t[i]]--;
         }
-        //Char appears in t, decrease count
-        for(auto x:t){
-            mp[x]--;
-        }
-        //Char with non-zero freq
-        for(auto x:count){
-            if(x.second!=0){
+        for (auto it : mpp)
+        {
+            if (it.second != 0)
                 return false;
-            }
         }
-        return true; 
+        return true;
     }
 };
 int main()
 {
-    Solution sol=Solution();
-    cout<<sol.isAnagram("anagram","nagaram");
+    Solution sol = Solution();
+    cout << sol.isAnagram("anagram", "nagaram");
 }
