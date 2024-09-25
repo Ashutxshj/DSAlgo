dd50f9eb902060d58b30aa0389973ecac1d224c4
Ashutosh Jha <ashutosh06066@gmail.com>
2024-09-25 14:27:09 +0530

    25-09

---
+++ LeetCode/ArrMaxSlidWin.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int maxElem(vector<int> lol)
+    {
+        int maxi = INT_MIN;
+        for (int i = 0; i < lol.size(); i++)
+        {
+            maxi = max(maxi, lol[i]);
+        }
+        return maxi;
+    }
+    vector<int> slicing(const vector<int> &pog, int x, int y)
+    {
+        return vector<int>(pog.begin() + x, pog.begin() + y + 1);
+    }
+    vector<int> maxSlidingWindow(vector<int> &nums, int k)
+    {
+        vector<int> arr;
+        int l = 0;
+        int r = k - 1;
+        while (r < nums.size())
+        {
+            arr.push_back(maxElem(slicing(nums, l, r)));
+            l++;
+            r++;
+        }
+        return arr;
+    }
+};
+int main()
+{
+    Solution s = Solution();
+    vector<int> ans = {1, 3, -1, -3, 5, 3, 6, 7};
+    vector<int> sol = s.maxSlidingWindow(ans, 3);
+    for (int i = 0; i < sol.size(); i++)
+    {
+        cout << sol[i] << " ";
+    }
+    // cout<<s.maxElem(ans);
+}
