f0c222c1e42585f31cf00a116638076615071951
Ashutosh Jha <ashutosh06066@gmail.com>
2024-09-27 15:48:53 +0530

    27-09

--- NeetCode/ArrayHashing/ContainsDuplicate.cpp
+++ NeetCode/ArrayHashing/ContainsDuplicate.cpp
@@ -1,22 +1,22 @@
-class Solution
-{
-public:
-    bool containsDuplicate(vector<int> &nums)
-    {
-        bool ans = false;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            for (int j = i + 1; j < nums.size(); j++)
-            {
-                if (nums[i] == nums[j])
-                {
-                    ans = true;
-                }
-            }
-        }
-        return ans;
-    }
-};
+// class Solution
+// {
+// public:
+//     bool containsDuplicate(vector<int> &nums)
+//     {
+//         bool ans = false;
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             for (int j = i + 1; j < nums.size(); j++)
+//             {
+//                 if (nums[i] == nums[j])
+//                 {
+//                     ans = true;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution
 {
 public:
@@ -25,7 +25,7 @@ public:
         unordered_set<int> st;
         for (int i = 0; i < nums.size(); i++)
         {
-            if (st.find(nums[i]) != st.end())
+            if (st.find(nums[i]) != st.end()) //If the element doesnt exist in the set
                 return true;
 
             st.insert(nums[i]);
--- NeetCode/ArrayHashing/GroupAnagrams.cpp
+++ NeetCode/ArrayHashing/GroupAnagrams.cpp
@@ -1,55 +1,57 @@
-class Solution
-{
-public:
-    bool isAnagram(string s, string t)
-    {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
-    }
-    bool isInArr(vector<vector<string>> s, vector<string> arr)
-    {
-        bool ans = false;
-        ans = find(s.begin(), s.end(), arr) != s.end();
-        return ans;
-    }
-    vector<vector<string>> groupAnagrams(vector<string> &strs)
-    {
-        vector<vector<string>> ans;
-        for (int i = 0; i < strs.size(); i++)
-        {
-            bool HasAnagram = false;
-            vector<string> part;
-            int j = 0;
-            while (j < strs.size())
-            {
-                if (isAnagram(strs[i], strs[j]))
-                {
-                    part.push_back(strs[j]);
-                    HasAnagram = true;
-                }
-                j++;
-            }
-            // for (int j = 0; j < strs.size(); j++)
-            // {
-            //     if (isAnagram(strs[i], strs[j]))
-            //     {
-            //         part.push_back(strs[j]);
-            //         HasAnagram = true;
-            //     }
-            // }
-            if (!part.empty() && !isInArr(ans, part))
-            {
-                ans.push_back(part);
-            }
-            if (!HasAnagram)
-            {
-                ans.push_back({strs[i]});
-            }
-        }
-        return ans;
-    }
-};
+// class Solution
+// {
+// public:
+//     bool isAnagram(string s, string t)
+//     {
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+//         return s == t;
+//     }
+//     bool isInArr(vector<vector<string>> s, vector<string> arr)
+//     {
+//         bool ans = false;
+//         ans = find(s.begin(), s.end(), arr) != s.end();
+//         return ans;
+//     }
+//     vector<vector<string>> groupAnagrams(vector<string> &strs)
+//     {
+//         vector<vector<string>> ans;
+//         for (int i = 0; i < strs.size(); i++)
+//         {
+//             bool HasAnagram = false;
+//             vector<string> part;
+//             int j = 0;
+//             while (j < strs.size())
+//             {
+//                 if (isAnagram(strs[i], strs[j]))
+//                 {
+//                     part.push_back(strs[j]);
+//                     HasAnagram = true;
+//                 }
+//                 j++;
+//             }
+//             // for (int j = 0; j < strs.size(); j++)
+//             // {
+//             //     if (isAnagram(strs[i], strs[j]))
+//             //     {
+//             //         part.push_back(strs[j]);
+//             //         HasAnagram = true;
+//             //     }
+//             // }
+//             if (!part.empty() && !isInArr(ans, part))
+//             {
+//                 ans.push_back(part);
+//             }
+//             if (!HasAnagram)
+//             {
+//                 ans.push_back({strs[i]});
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+//! Two words are anagrams if they contain the same characters in the same frequencies, even though their order might differ.
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -60,15 +62,12 @@ public:
             for (char c : s) {
                 count[c - 'a']++;
             }
-
             string key;
             for (int i : count) {
                 key += "#" + to_string(i);
             }
-
             ans[key].push_back(s);
         }
-
         vector<vector<string>> result;
         for (auto& pair : ans) {
             result.push_back(pair.second);
