2dad9a2b06fda895802ee98c0e914959eba0571d
Ashutosh Jha <ashutosh06066@gmail.com>
2024-10-18 11:53:51 +0530

    18-10

--- NeetCode/Trees/IsSubTree.cpp
+++ NeetCode/Trees/IsSubTree.cpp
@@ -1,24 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
 class Solution {
 public:
-    bool SameTree(TreeNode* root,TreeNode* subRoot){
-        if(!subRoot){
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!subRoot) {
             return true;
         }
-        if(!root){
+        if (!root) {
             return false;
         }
-        if(SameTree(root,subRoot)){
+
+        if (sameTree(root, subRoot)) {
             return true;
         }
-        return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        //Recursion
-        if(!root && !subRoot){
+
+    bool sameTree(TreeNode* root, TreeNode* subRoot) {
+        if (!root && !subRoot) {
             return true;
         }
-        if(root && subRoot && root->val==subRoot->val){
-            return SameTree(root->right,subRoot->right) && SameTree(root->left,root->left);
+        if (root && subRoot && root->val == subRoot->val) {
+            return sameTree(root->left, subRoot->left) && sameTree(root->right, subRoot->right);
         }
         return false;
     }
