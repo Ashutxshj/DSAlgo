7f13e3efc990635fc0138a5839fc33b88c317af3
Ashutosh Jha <ashutosh06066@gmail.com>
2024-11-18 18:09:08 +0530

    18-11

--- NeetCode/ArrayHashing/TopKFrequentElements.cpp
+++ NeetCode/ArrayHashing/TopKFrequentElements.cpp
@@ -1,18 +1,24 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        if(nums.size()==1) return nums;
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        if (nums.size() == 1)
+            return nums;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++)
+        {
             mp[nums[i]]++;
         }
-        vector<pair<int,int>>ans;
-        for(const auto&i:mp){
-            ans.push_back({i.second,i.first});
+        vector<pair<int, int>> ans;
+        for (const auto &i : mp)
+        {
+            ans.push_back({i.second, i.first});
         }
-        sort(ans.rbegin(),ans.rend());
-        vector<int>res;
-        for(int i=0;i<k;i++){
+        sort(ans.rbegin(), ans.rend());
+        vector<int> res;
+        for (int i = 0; i < k; i++)
+        {
             res.push_back(ans[i].second);
         }
         return res;
