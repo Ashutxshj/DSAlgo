d758f1ebf7f3e26ea34599d65af5daffdc42ff18
Ashutosh Jha <ashutosh06066@gmail.com>
2024-11-19 12:20:15 +0530

    19-11

--- NeetCode/ArrayHashing/LongestConsecutiveSequence.cpp
+++ NeetCode/ArrayHashing/LongestConsecutiveSequence.cpp
@@ -1,16 +1,21 @@
-class Solution {
+class Solution
+{
 public:
-    int longestConsecutive(vector<int>& nums) {
-        unordered_set<int>st(nums.begin(),nums.end());
-        int ans=0;
-        
-        for(int n:st){
-            if(st.find(n-1)==st.end()){
-                int len=1;
-                while(st.find(n+len)!=st.end()){
+    int longestConsecutive(vector<int> &nums)
+    {
+        unordered_set<int> st(nums.begin(), nums.end());
+        int ans = 0;
+
+        for (int n : st)
+        {
+            if (st.find(n - 1) == st.end())
+            {
+                int len = 1;
+                while (st.find(n + len) != st.end())
+                {
                     len++;
                 }
-                ans=max(len,ans);
+                ans = max(len, ans);
             }
         }
         return ans;
