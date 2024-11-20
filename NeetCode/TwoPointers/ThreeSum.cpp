2f1a7859458640f6a4af2d53ec7001ae5022dc24
Ashutosh Jha <ashutosh06066@gmail.com>
2024-11-20 15:43:41 +0530

    20-11

--- NeetCode/TwoPointers/ThreeSum.cpp
+++ NeetCode/TwoPointers/ThreeSum.cpp
@@ -1,32 +1,30 @@
-class Solution
-{
+class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int> &nums)
-    {
-        vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int j=i+1;
-            int k=nums.size()-1;
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum<0){
-                    j++;
-                }
-                else if(sum>0){
-                    k--;
-                }
-                else{
-                    vector<int> temp={nums[i],nums[j],nums[k]};
-                    ans.push_back(temp);
-                    j++;
-                    k--;
-                    while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k && nums[k]==nums[k+1]) k--;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int l = i + 1, r = nums.size() - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0) {
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    res.push_back({nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
                 }
             }
         }
-        return ans;
+        return res;
     }
 };
