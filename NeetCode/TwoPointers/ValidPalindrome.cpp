02f9eb289c85e881f402309373ae980764408daa
Ashutosh Jha <ashutosh06066@gmail.com>
2024-11-20 10:28:42 +0530

    20-11

--- NeetCode/TwoPointers/ValidPalindrome.cpp
+++ NeetCode/TwoPointers/ValidPalindrome.cpp
@@ -16,3 +16,15 @@ public:
         return true;
     }
 };
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string t="";
+        for(char c:s){
+            if(isalnum(c)){
+                t+=tolower(c);
+            }
+        }
+        return t==string(t.rbegin(),t.rend());
+    }
+};
