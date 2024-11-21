1ca76d237a999ef2eef4ec7425e4372e061c1811
Ashutosh Jha <ashutosh06066@gmail.com>
2024-11-21 15:59:24 +0530

    21-11

--- NeetCode/SlidingWindow/SlidWindowMax.cpp
+++ NeetCode/SlidingWindow/SlidWindowMax.cpp
@@ -1,53 +1,18 @@
-class Solution
-{
-public:
-    int maxElem(vector<int> lol)
-    {
-        int maxi = INT_MIN;
-        for (int i = 0; i < lol.size(); i++)
-        {
-            maxi = max(maxi, lol[i]);
-        }
-        return maxi;
-    }
-    vector<int> slicing(const vector<int> &pog, int x, int y)
-    {
-        return vector<int>(pog.begin() + x, pog.begin() + y + 1);
-    }
-    vector<int> maxSlidingWindow(vector<int> &nums, int k)
-    {
-        vector<int> arr;
-        int l = 0;
-        int r = k - 1;
-        while (r < nums.size())
-        {
-            arr.push_back(maxElem(vector<int>(nums.begin() + l, nums.begin() + r + 1)));
-            l++;
-            r++;
-        }
-        return arr;
-    }
-};
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> output(nums.size()-k+1);
-        deque<int> q;
-        int l=0,r=0;
-        while(r<nums.size()){
-            while(!q.empty() && nums[q.back()] <nums[r]){
-                q.pop_back();
-            }
-            q.push_back(r);
-            if(l>q.front()){
-                q.pop_front();
-            }
-            if(r+1>=k){
-                output[l]=nums[q.front()];
-                l++;
-            }
-            r++;
+    int maxElem(vector<int>lol){
+        int as=INT_MIN;
+        for(int i=0;i<lol.size();i++){
+            as=max(as,lol[i]);
         }
-        return output;
+        return as;
+    }
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int>ans;
+        for(int i=0;i<nums.size()-k+1;i++)
+        {
+            ans.push_back(maxElem(vector<int>(nums.begin()+i,nums.begin()+i+k)));
+        }
+        return ans;
     }
 };
