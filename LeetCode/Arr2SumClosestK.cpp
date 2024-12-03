08cb0df513db38cdcf15812c2226d29868a460f8
Ashutosh Jha <ashutosh06066@gmail.com>
2024-12-03 21:11:19 +0530

    3-12

--- LeetCode/Arr2SumClosestK.cpp
+++ LeetCode/Arr2SumClosestK.cpp
@@ -1,23 +1,27 @@
-class Solution {
-  public:
-    vector<int> sumClosest(vector<int>& arr, int k) {
-        sort(arr.begin(),arr.end());
+class Solution
+{
+public:
+    vector<int> sumClosest(vector<int> &arr, int k)
+    {
+        sort(arr.begin(), arr.end());
         vector<int> result(2);
-        int minDiff=INT_MAX;
-        
-        int l=0,r=arr.size()-1;
-        while(l<r){
-            int sum=arr[l]+arr[r];
-            int diff=abs(k-sum);
-            
-            if(diff<minDiff){
-                minDiff=diff;
-                result[0]=arr[l];
-                result[1]=arr[r];
+        int minDiff = INT_MAX;
+
+        int l = 0, r = arr.size() - 1;
+        while (l < r)
+        {
+            int sum = arr[l] + arr[r];
+            int diff = abs(k - sum);
+
+            if (diff < minDiff)
+            {
+                minDiff = diff;
+                result[0] = arr[l];
+                result[1] = arr[r];
             }
-            if(sum<k)
+            if (sum < k)
                 l++;
-            else if(sum>k)
+            else if (sum > k)
                 r--;
             else
                 break;
