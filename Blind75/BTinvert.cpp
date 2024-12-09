8f7d664c56c881c8cc89db86404f1816da60fd57
Ashutosh Jha <ashutosh06066@gmail.com>
2024-12-09 11:19:28 +0530

    09-12

---
+++ Blind75/BTinvert.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==nullptr)
+            return root;
+        TreeNode* lol=new TreeNode(root->val);
+        lol->left=invertTree(root->right);
+        lol->right=invertTree(root->left);
+        return lol;
+    }
+};
