cc6bf478e79416fc4cc45790e11059ba1052bfdf
Ashutosh Jha <ashutosh06066@gmail.com>
2025-01-23 10:31:41 +0530

    23-01

--- NeetCode/SlidingWindow/LongestSubstrWithoutRepeatingChar.cpp
+++ NeetCode/SlidingWindow/LongestSubstrWithoutRepeatingChar.cpp
@@ -42,3 +42,21 @@ public:
         return ans;
     }
 };
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        unordered_map<char, int> mp;
+        int i = 0, ans = 0;
+        for (int j = 0; j < s.length(); j++)
+        {
+            if (mp.find(s[j]) != mp.end() && mp[s[j]] >= i)
+                i = mp[s[j]] + 1;
+
+            mp[s[j]] = j;
+            ans = max(ans, j - i + 1);
+        }
+        return ans;
+    }
+};
