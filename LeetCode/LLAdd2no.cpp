b05c18f9d6240c37748aa8baf83209b337d59d76
Ashutosh Jha <ashutosh06066@gmail.com>
2025-01-31 21:18:45 +0530

    31-01

--- LeetCode/LLAdd2no.cpp
+++ LeetCode/LLAdd2no.cpp
@@ -8,34 +8,26 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-#include <iostream>
-using namespace std;
-class Solution
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *dummy = new ListNode(0);
-        ListNode *curr = dummy;
-        int carry = 0;
-        while(l1 || l2 || carry == 1){
-            int sum = 0;
+        int carry=0;
+        ListNode* temp=new ListNode(0);
+        ListNode* curr=temp;
+        while(l1 || l2 || carry){
+            int sum=carry;
             if(l1){
-                sum += l1->val;
-                l1 = l1->next;
+                sum+=l1->val;
+                l1=l1->next;
             }
             if(l2){
-                sum += l2->val;
-                l2 = l2->next;
+                sum+=l2->val;
+                l2=l2->next;
             }
-            sum += carry;
-            carry = sum/10;
-            ListNode *node = new ListNode(sum % 10);
-            curr->next = node;
-            curr = curr->next;
+            carry=sum/10;
+            curr->next=new ListNode(sum%10);
+            curr=curr->next;
         }
-        return dummy->next;
+        return temp->next;
     }
 };
-int main()
-{
-}
