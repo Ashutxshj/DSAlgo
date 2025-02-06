0811eba01b7419da1fd7d0e49be859548d0d18f2
Ashutosh Jha <ashutosh06066@gmail.com>
2025-02-06 12:00:08 +0530

    06-02

--- 01Knapsack.cpp
+++ 01Knapsack.cpp
@@ -50,3 +50,27 @@ public:
         return solve(capacity, val, wt, n, t);
     }
 };
+// Top Down Approach
+class Solution
+{
+public:
+    int knapSack(int w, vector<int> &val, vector<int> &wt)
+    {
+        int n = val.size();
+        int t[n + 1][w + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= w; j++)
+            {
+                if (i == 0 || j == 0)
+                    t[i][j] = 0;
+                else if (wt[i - 1] <= j)
+                    t[i][j] = max(val[i - 1] + t[i - 1][j - wt[i - 1]], t[i - 1][j]);
+
+                else
+                    t[i][j] = t[i - 1][j];
+            }
+        }
+        return t[n][w];
+    }
+};
