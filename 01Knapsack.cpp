c008cfacc3e8695396ce37807126f58674e8a6a6
Ashutosh Jha <ashutosh06066@gmail.com>
2025-02-06 11:29:12 +0530

    06-02

--- 01Knapsack.cpp
+++ 01Knapsack.cpp
@@ -1,20 +1,52 @@
-//Recursive
-class Solution {
-  public:
-    int solve(int w, vector<int> &val, vector<int>&wt,int n)
+// Recursive
+class Solution
+{
+public:
+    int solve(int w, vector<int> &val, vector<int> &wt, int n)
     {
-        if(w==0 || n==0) return 0;
-        
-        if(w>=wt[n-1])
-            return max(val[n-1]+solve(w-wt[n-1],val,wt,n-1),solve(w,val,wt,n-1));
-        
+        if (w == 0 || n == 0)
+            return 0;
+
+        if (w >= wt[n - 1])
+            return max(val[n - 1] + solve(w - wt[n - 1], val, wt, n - 1), solve(w, val, wt, n - 1));
+
         else
-            return solve(w,val,wt,n-1);
-        
+            return solve(w, val, wt, n - 1);
     }
     int knapSack(int capacity, vector<int> &val, vector<int> &wt)
     {
         int n = val.size();
-        return solve(capacity,val,wt,n);
+        return solve(capacity, val, wt, n);
+    }
+};
+
+// Memoization
+class Solution
+{
+public:
+    int solve(int w, vector<int> &val, vector<int> &wt, int n, vector<vector<int>> &t)
+    {
+        if (w == 0 || n == 0)
+            return 0;
+
+        if (t[w][n] != -1)
+            return t[w][n];
+
+        if (w >= wt[n - 1])
+        {
+            t[w][n] = max(val[n - 1] + solve(w - wt[n - 1], val, wt, n - 1, t), solve(w, val, wt, n - 1, t));
+        }
+
+        else
+        {
+            t[w][n] = solve(w, val, wt, n - 1, t);
+        }
+        return t[w][n];
+    }
+    int knapSack(int capacity, vector<int> &val, vector<int> &wt)
+    {
+        int n = val.size();
+        vector<vector<int>> t(capacity + 1, vector<int>(n + 1, -1));
+        return solve(capacity, val, wt, n, t);
     }
 };
